284ed81f738e54d8398447a6a7834c05dff1bcd7
Ricz124 <ricardobegali87@gmail.com>
2024-10-23 16:05:45 -0300

    caminhos workpace

--- RINGGWebApp/workstation.php
+++ RINGGWebApp/workstation.php
@@ -6,28 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Workspace</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="./css/pagIndex.css">
+    <link rel="stylesheet" href="../css/pagIndex.css">
     <script src="script.js" defer></script>
 </head>
 <body>
     <nav>
-      <div class="icone-ringg"><a href="index.html"><img src="./img/img2.jpg"></a></div>
+      <div class="icone-ringg"><a href="../index.html"><img src="../img/img2.jpg"></a></div>
       <div class="nav-mob">
         <ul>
-          <a href="./quemsomos.html"><li><i class="fa-solid fa-question"></i></li></a>
-          <a href="./ajuda.html"><li><i class="fa-solid fa-circle-info"></i></li></a>
+          <a href="../quemsomos.html"><li><i class="fa-solid fa-question"></i></li></a>
+          <a href="../ajuda.html"><li><i class="fa-solid fa-circle-info"></i></li></a>
           <a href="workstation.php"><li><i class="fa-solid fa-laptop"></i></li></a>
-          <a href="php/index.php"><li><i class="fa-solid fa-user"></i></li></a>
+          <a href="php/dashboard.php"><li><i class="fa-solid fa-user"></i></li></a>
         </ul>
       </div>
       <div class="navbar">
         <div class="op-nav">
           <ul>
-            <a href="quemsomos.html"><li>Quem Somos</li></a>
-            <a href="./RINGGWebApp/php/dashboard.php"><li>Dashboard</li></a>
-            <a href="ajuda.html"><li>Ajuda</li></a>
-            <a href="RINGGWebApp/workstation.php"><li>Espaço de Trabalho</li></a>
-            <a href="./RINGGWebApp/php/index.php"><li>ENTRAR</li></a>
+            <a href="../quemsomos.html"><li>Quem Somos</li></a>
+            <a href="php/dashboard.php"><li>Dashboard</li></a>
+            <a href="../ajuda.html"><li>Ajuda</li></a>
+            <a href="workstation.php"><li>Espaço de Trabalho</li></a>
+            <a href="php/index.php"><li>ENTRAR</li></a>
           </ul>
         </div>
       </div>
