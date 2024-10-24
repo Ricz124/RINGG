6622551f0b5539f01648ffb9866355a78201c55d
Ricz124 <ricardobegali87@gmail.com>
2024-10-23 21:26:24 -0300

    dsdsd

--- RINGGWebApp/php/remove_section.php
+++ RINGGWebApp/php/remove_section.php
@@ -1,20 +1,39 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 include '../session_start.php';
 require 'db_connections.php';
 
 // Verifique se o método da requisição é POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Recebe o corpo da requisição
-    $data = json_decode(file_get_contents("php://input"), true);
-    $sectionId = $data['sectionId']; // Obtenha o ID da seção
+    // Obtém o corpo da requisição
+    $input = file_get_contents('php://input');
+    $data = json_decode($input, true); // Decodifica JSON em array
 
-    // Verifique se o ID está definido
-    if (isset($sectionId)) {
+    // Verifica se o json_decode retornou um array
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        echo json_encode([
+            "status" => "error",
+            "message" => "JSON inválido."
+        ]);
+        exit;
+    }
+
+    // Obtém o ID da seção
+    $sectionId = $data['sectionId'] ?? null; // Seção pode não existir
+
+    if ($sectionId) {
         // Execute sua lógica de remoção
         $query = "DELETE FROM sections WHERE id = ?";
         $stmt = $conn->prepare($query);
+
+        // Verifique se a preparação da consulta foi bem-sucedida
+        if ($stmt === false) {
+            echo json_encode([
+                "status" => "error",
+                "message" => "Erro na preparação da consulta."
+            ]);
+            exit;
+        }
+
         $stmt->bind_param("i", $sectionId);
         $success = $stmt->execute();
 
